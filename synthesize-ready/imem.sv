49793a05cdb9cefc9fff093d6b44a3f0c0001d4d
YoussefMouaddib <76605804+YoussefMouaddib@users.noreply.github.com>
2026-03-18 18:08:58 -0500

    Update imem.sv

--- synthesize-ready/imem.sv
+++ synthesize-ready/imem.sv
@@ -39,61 +39,61 @@ module inst_rom #(
             end
 
         // UART Hello World Program
-            rom['h00] = 32'h20200001;  // ADDI X1, X0, #1
-            rom['h01] = 32'h00210401;  // LSL  X1, X1, #16
-            rom['h02] = 32'h20410004;  // ADDI X2, X1, #4
-            rom['h03] = 32'h20A00001;  // ADDI X5, X0, #1
-            rom['h04] = 32'h21200002;  // ADDI X9, X0, #2
-            rom['h05] = 32'h01290301;  // LSL  X9, X9, #12
-            rom['h06] = 32'h20C00048;  // ADDI X6, X0, #72
-            rom['h07] = 32'h44C90000;  // STR  X6, [X9, #0]
-            rom['h08] = 32'h20C00065;  // ADDI X6, X0, #101
-            rom['h09] = 32'h44C90004;  // STR  X6, [X9, #4]
-            rom['h0A] = 32'h20C0006C;  // ADDI X6, X0, #108
-            rom['h0B] = 32'h44C90008;  // STR  X6, [X9, #8]
-            rom['h0C] = 32'h20C0006C;  // ADDI X6, X0, #108
-            rom['h0D] = 32'h44C9000C;  // STR  X6, [X9, #12]
-            rom['h0E] = 32'h20C0006F;  // ADDI X6, X0, #111
-            rom['h0F] = 32'h44C90010;  // STR  X6, [X9, #16]
-            rom['h10] = 32'h20C0002C;  // ADDI X6, X0, #44
-            rom['h11] = 32'h44C90014;  // STR  X6, [X9, #20]
-            rom['h12] = 32'h20C00020;  // ADDI X6, X0, #32
-            rom['h13] = 32'h44C90018;  // STR  X6, [X9, #24]
-            rom['h14] = 32'h20C0004C;  // ADDI X6, X0, #76
-            rom['h15] = 32'h44C9001C;  // STR  X6, [X9, #28]
-            rom['h16] = 32'h20C00045;  // ADDI X6, X0, #69
-            rom['h17] = 32'h44C90020;  // STR  X6, [X9, #32]
-            rom['h18] = 32'h20C00047;  // ADDI X6, X0, #71
-            rom['h19] = 32'h44C90024;  // STR  X6, [X9, #36]
-            rom['h1A] = 32'h20C00076;  // ADDI X6, X0, #118
-            rom['h1B] = 32'h44C90028;  // STR  X6, [X9, #40]
-            rom['h1C] = 32'h20C00038;  // ADDI X6, X0, #56
-            rom['h1D] = 32'h44C9002C;  // STR  X6, [X9, #44]
-            rom['h1E] = 32'h20C00021;  // ADDI X6, X0, #33
-            rom['h1F] = 32'h44C90030;  // STR  X6, [X9, #48]
-            rom['h20] = 32'h20C0000D;  // ADDI X6, X0, #13
-            rom['h21] = 32'h44C90034;  // STR  X6, [X9, #52]
-            rom['h22] = 32'h20C0000A;  // ADDI X6, X0, #10
-            rom['h23] = 32'h44C90038;  // STR  X6, [X9, #56]
-            rom['h24] = 32'h20C00000;  // ADDI X6, X0, #0
-            rom['h25] = 32'h44C9003C;  // STR  X6, [X9, #60]
-            rom['h26] = 32'h20690000;  // ADDI X3, X9, #0
+            rom['h00] <= 32'h20200001;  // ADDI X1, X0, #1
+            rom['h01] <= 32'h00210401;  // LSL  X1, X1, #16
+            rom['h02] <= 32'h20410004;  // ADDI X2, X1, #4
+            rom['h03] <= 32'h20A00001;  // ADDI X5, X0, #1
+            rom['h04] <= 32'h21200002;  // ADDI X9, X0, #2
+            rom['h05] <= 32'h01290301;  // LSL  X9, X9, #12
+            rom['h06] <= 32'h20C00048;  // ADDI X6, X0, #72
+            rom['h07] <= 32'h44C90000;  // STR  X6, [X9, #0]
+            rom['h08] <= 32'h20C00065;  // ADDI X6, X0, #101
+            rom['h09] <= 32'h44C90004;  // STR  X6, [X9, #4]
+            rom['h0A] <= 32'h20C0006C;  // ADDI X6, X0, #108
+            rom['h0B] <= 32'h44C90008;  // STR  X6, [X9, #8]
+            rom['h0C] <= 32'h20C0006C;  // ADDI X6, X0, #108
+            rom['h0D] <= 32'h44C9000C;  // STR  X6, [X9, #12]
+            rom['h0E] <= 32'h20C0006F;  // ADDI X6, X0, #111
+            rom['h0F] <= 32'h44C90010;  // STR  X6, [X9, #16]
+            rom['h10] <= 32'h20C0002C;  // ADDI X6, X0, #44
+            rom['h11] <= 32'h44C90014;  // STR  X6, [X9, #20]
+            rom['h12] <= 32'h20C00020;  // ADDI X6, X0, #32
+            rom['h13] <= 32'h44C90018;  // STR  X6, [X9, #24]
+            rom['h14] <= 32'h20C0004C;  // ADDI X6, X0, #76
+            rom['h15] <= 32'h44C9001C;  // STR  X6, [X9, #28]
+            rom['h16] <= 32'h20C00045;  // ADDI X6, X0, #69
+            rom['h17] <= 32'h44C90020;  // STR  X6, [X9, #32]
+            rom['h18] <= 32'h20C00047;  // ADDI X6, X0, #71
+            rom['h19] <= 32'h44C90024;  // STR  X6, [X9, #36]
+            rom['h1A] <= 32'h20C00076;  // ADDI X6, X0, #118
+            rom['h1B] <= 32'h44C90028;  // STR  X6, [X9, #40]
+            rom['h1C] <= 32'h20C00038;  // ADDI X6, X0, #56
+            rom['h1D] <= 32'h44C9002C;  // STR  X6, [X9, #44]
+            rom['h1E] <= 32'h20C00021;  // ADDI X6, X0, #33
+            rom['h1F] <= 32'h44C90030;  // STR  X6, [X9, #48]
+            rom['h20] <= 32'h20C0000D;  // ADDI X6, X0, #13
+            rom['h21] <= 32'h44C90034;  // STR  X6, [X9, #52]
+            rom['h22] <= 32'h20C0000A;  // ADDI X6, X0, #10
+            rom['h23] <= 32'h44C90038;  // STR  X6, [X9, #56]
+            rom['h24] <= 32'h20C00000;  // ADDI X6, X0, #0
+            rom['h25] <= 32'h44C9003C;  // STR  X6, [X9, #60]
+            rom['h26] <= 32'h20690000;  // ADDI X3, X9, #0
             
             // send_string loop
-            rom['h27] = 32'h40830000;  // LDR  X4, [X3, #0]
-            rom['h28] = 32'h60800007;  // CBZ  X4, done (PC+28)
+            rom['h27] <= 32'h40830000;  // LDR  X4, [X3, #0]
+            rom['h28] <= 32'h60800007;  // CBZ  X4, done (PC+28)
             
             // wait_tx loop
-            rom['h29] = 32'h40C20000;  // LDR  X6, [X2, #0]
-            rom['h2A] = 32'h00C62824;  // AND  X6, X6, X5
-            rom['h2B] = 32'h64DFFFFE;  // CBNZ X6, wait_tx (PC-8)
+            rom['h29] <= 32'h40C20000;  // LDR  X6, [X2, #0]
+            rom['h2A] <= 32'h00C62824;  // AND  X6, X6, X5
+            rom['h2B] <= 32'h64DFFFFE;  // CBNZ X6, wait_tx (PC-8)
             
-            rom['h2C] = 32'h44810000;  // STR  X4, [X1, #0]
-            rom['h2D] = 32'h20630004;  // ADDI X3, X3, #4
-            rom['h2E] = 32'h83FFFFF9;  // B    send_string (PC-28)
+            rom['h2C] <= 32'h44810000;  // STR  X4, [X1, #0]
+            rom['h2D] <= 32'h20630004;  // ADDI X3, X3, #4
+            rom['h2E] <= 32'h83FFFFF9;  // B    send_string (PC-28)
             
             // done
-            rom['h2F] = 32'h80000000;  // B    done (infinite loop)
+            rom['h2F] <= 32'h80000000;  // B    done (infinite loop)
         end
             
     end 
