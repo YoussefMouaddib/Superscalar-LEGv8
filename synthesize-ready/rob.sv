a59c79c2ddc41c28241c80e3e0687aa74eda8583
YoussefMouaddib <76605804+YoussefMouaddib@users.noreply.github.com>
2026-03-19 17:41:33 -0500

    Update rob.sv

--- synthesize-ready/rob.sv
+++ synthesize-ready/rob.sv
@@ -2,7 +2,7 @@
 import core_pkg::*;
 
 module rob #(
-  parameter int ROB_SIZE = 32
+  parameter int ROB_SIZE = 16
 )(
   input  logic                    clk,
   input  logic                    reset,
