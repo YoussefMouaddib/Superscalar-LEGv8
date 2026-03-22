377b87370e9b8689a613176bf1d565b4cfe9d30f
YoussefMouaddib <76605804+YoussefMouaddib@users.noreply.github.com>
2026-03-22 00:34:19 -0500

    Update uart_stub.sv'

--- synthesize-ready/uart_stub.sv
+++ synthesize-ready/uart_stub.sv
@@ -29,7 +29,7 @@ module uart_stub (
         end else begin
             tx_data_reg <= write_data[7:0];
             // Print transmitted character
-            $display("UART WRITE: %c", write_data[7:0]);
+            $display("UART WRITE: %c",tx_data_reg );
             
         end
     end
