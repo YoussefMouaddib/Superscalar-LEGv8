b0cb63b59300ddbb4b0420e3ebe33cbf215cbf99
YoussefMouaddib <76605804+YoussefMouaddib@users.noreply.github.com>
2026-07-24 21:24:21 -0500

    Create bind.sv

---
+++ Hardware/UVM Freelist/bind.sv
@@ -0,0 +1,14 @@
+bind free_list freelist_fv #(
+  .PHYS_REGS(PHYS_REGS),
+  .RENAME_START(RENAME_START),
+  .ALLOC_PORTS(ALLOC_PORTS),
+  .FREE_PORTS(FREE_PORTS)
+) fv_inst (
+  .clk(clk),
+  .reset(reset),
+  .alloc_en(alloc_en),
+  .alloc_phys(alloc_phys),
+  .alloc_valid(alloc_valid),
+  .free_en(free_en),
+  .free_phys(free_phys)
+);
