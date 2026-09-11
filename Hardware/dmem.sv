95f006c4c7cc23605d567f7bcd407c888c962b65
YoussefMouaddib <76605804+YoussefMouaddib@users.noreply.github.com>
2026-09-11 16:33:00 -0500

    Update dmem.sv

--- Hardware/dmem.sv
+++ Hardware/dmem.sv
@@ -2,7 +2,7 @@
 import core_pkg::*;
 
 module data_scratchpad #(
-    parameter int MEM_SIZE = 512,     
+    parameter int MEM_SIZE = 4096,     
     parameter int XLEN = 32
 )(
     input  logic             clk,
