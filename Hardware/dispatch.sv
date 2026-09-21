2ab5a2373fd7bc1688e554b53a41a175eef8d68a
YoussefMouaddib <76605804+YoussefMouaddib@users.noreply.github.com>
2026-09-21 17:37:34 -0500

    Update dispatch.sv

--- Hardware/dispatch.sv
+++ Hardware/dispatch.sv
@@ -371,7 +371,7 @@ module dispatch #(
             // RS Allocation
             // ====================================================
             for (int i = 0; i < FETCH_W; i++) begin
-                if (rename_valid_r[i] && (rename_is_alu_r[i] || rename_is_branch_r[i]) &&
+                if (rename_valid_r[i] &&
                     !(rename_is_alu_r[i] && rename_prd_r[i] == 6'd0)) begin
                     rs_alloc_en[i] <= 1'b1;
                     rs_alloc_dst_tag[i] <= rename_prd_r[i];
