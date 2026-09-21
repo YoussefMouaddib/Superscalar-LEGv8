e03ede7fb09c504530340b3de6dda1c90762d671
YoussefMouaddib <76605804+YoussefMouaddib@users.noreply.github.com>
2026-09-21 12:36:03 -0500

    Update dmem.sv

--- Hardware/dmem.sv
+++ Hardware/dmem.sv
@@ -39,7 +39,40 @@ module data_scratchpad #(
         for (int i = 0; i < (MEM_SIZE/4); i++) begin
             mem[i] = '0;
         end
-        mem[4] = 32'd67;  // Test data at address 16 (0x10)
+         // String data starting at byte address 52 (word index 13)
+    // "Superscalar LEGv8 Kernel is ON"
+    mem[13] = 32'h00000053;  // 'S'  (addr 52)
+    mem[14] = 32'h00000075;  // 'u'  (addr 56)
+    mem[15] = 32'h00000070;  // 'p'  (addr 60)
+    mem[16] = 32'h00000065;  // 'e'  (addr 64)
+    mem[17] = 32'h00000072;  // 'r'  (addr 68)
+    mem[18] = 32'h00000073;  // 's'  (addr 72)
+    mem[19] = 32'h00000063;  // 'c'  (addr 76)
+    mem[20] = 32'h00000061;  // 'a'  (addr 80)
+    mem[21] = 32'h0000006C;  // 'l'  (addr 84)
+    mem[22] = 32'h00000061;  // 'a'  (addr 88)
+    mem[23] = 32'h00000072;  // 'r'  (addr 92)
+    mem[24] = 32'h00000020;  // ' '  (addr 96)
+    mem[25] = 32'h0000004C;  // 'L'  (addr 100)
+    mem[26] = 32'h00000045;  // 'E'  (addr 104)
+    mem[27] = 32'h00000047;  // 'G'  (addr 108)
+    mem[28] = 32'h00000076;  // 'v'  (addr 112)
+    mem[29] = 32'h00000038;  // '8'  (addr 116)
+    mem[30] = 32'h00000020;  // ' '  (addr 120)
+    mem[31] = 32'h0000004B;  // 'K'  (addr 124)
+    mem[32] = 32'h00000065;  // 'e'  (addr 128)
+    mem[33] = 32'h00000072;  // 'r'  (addr 132)
+    mem[34] = 32'h0000006E;  // 'n'  (addr 136)
+    mem[35] = 32'h00000065;  // 'e'  (addr 140)
+    mem[36] = 32'h0000006C;  // 'l'  (addr 144)
+    mem[37] = 32'h00000020;  // ' '  (addr 148)
+    mem[38] = 32'h00000069;  // 'i'  (addr 152)
+    mem[39] = 32'h00000073;  // 's'  (addr 156)
+    mem[40] = 32'h00000020;  // ' '  (addr 160)
+    mem[41] = 32'h0000004F;  // 'O'  (addr 164)
+    mem[42] = 32'h0000004E;  // 'N'  (addr 168)
+    mem[43] = 32'h00000000;  // '\0' (addr 172)
+end
     end
     
     // ============================================================
