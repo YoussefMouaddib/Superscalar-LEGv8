76150eebd76924f139fc44170fefdde157b282c5
YoussefMouaddib <76605804+YoussefMouaddib@users.noreply.github.com>
2026-09-22 18:55:22 -0500

    Update dispatch.sv

--- Hardware/dispatch.sv
+++ Hardware/dispatch.sv
@@ -395,7 +395,8 @@ module dispatch #(
             // ROB Allocation
             // ====================================================
             for (int i = 0; i < FETCH_W; i++) begin
-                if (rename_valid_r[i] && !(rename_is_alu_r[i] && rename_prd_r[i] == 6'd0)) begin
+                if (rename_valid_r[i] && !(rename_is_alu_r[i] && rename_prd_r[i] == 6'd0)  
+                    && !(rename_prd_r[i] == 6'd0 && rename_prs1_r[i] < 6'd30 && !(rename_opcode_r[i] == 6'd32 || rename_opcode_r[i] == 6'd33))) begin
                     rob_alloc_en[i] <= 1'b1;
                     rob_alloc_is_store[i] <= rename_is_store_r[i];
                     rob_alloc_is_load[i] <= rename_is_load_r[i];
