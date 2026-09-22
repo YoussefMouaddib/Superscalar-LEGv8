04e068ea997b0cec596e500bd4f6bce008e701a2
YoussefMouaddib <76605804+YoussefMouaddib@users.noreply.github.com>
2026-09-22 18:48:26 -0500

    Update dispatch.sv

--- Hardware/dispatch.sv
+++ Hardware/dispatch.sv
@@ -373,7 +373,7 @@ module dispatch #(
             for (int i = 0; i < FETCH_W; i++) begin
                 if (rename_valid_r[i] &&
                     !(rename_is_alu_r[i] && rename_prd_r[i] == 6'd0)
-                    && !(rename_prd_r[i] == 6'd0 && rename_prs1_r[i] < 6'd30 && (rename_opcode_r[i] !== 6'd32 || rename_opcode_r[i] !== 6'd33))) begin
+                    && !(rename_prd_r[i] == 6'd0 && rename_prs1_r[i] < 6'd30 && !(rename_opcode_r[i] == 6'd32 || rename_opcode_r[i] == 6'd33))) begin
                     rs_alloc_en[i] <= 1'b1;
                     rs_alloc_dst_tag[i] <= rename_prd_r[i];
                     rs_alloc_src1_tag[i] <= rename_prs1_r[i];
