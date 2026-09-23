0db3957cceccf5860a08ed513a80c00da6a1740d
YoussefMouaddib <76605804+YoussefMouaddib@users.noreply.github.com>
2026-09-23 07:06:36 -0500

    Update ooo_core_top_new.sv

--- Hardware/ooo_core_top_new.sv
+++ Hardware/ooo_core_top_new.sv
@@ -623,9 +623,9 @@ module ooo_core_top (
     data_scratchpad dmem (
         .clk(clk_core), .reset(sys_reset),
         .mem_req(mem_req),
-        .mem_we(scratchpad_we), .mem_addr(scratchpad_addr), .mem_wdata(scratchpad_wdata),
+        .mem_we(mem_we), .mem_addr(scratchpad_addr), .mem_wdata(scratchpad_wdata),
         .mem_size(2'b10), .mem_atomic(1'b0), .mem_cmp_val('0),
-        .mem_ready(scratchpad_ready), .mem_rdata(scratchpad_rdata), .mem_error(mem_error)
+        .mem_ready(mem_ready), .mem_rdata(mem_rdata), .mem_error(mem_error)
     );
 
     // ================================================================
